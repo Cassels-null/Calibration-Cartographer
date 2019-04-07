9678a94c7028c62b4fe75896fc994c0f19b76dfd
Cassels-null <42cassels@gmail.com>
2019-04-07 16:14:44 -0400

    Bugfix: corrected evaluation buttons to score correct predictions

--- client/src/list.jsx
+++ client/src/list.jsx
@@ -16,14 +16,18 @@ class List extends React.Component {
     }
 
     render(){
-        this.props.predictions.sort((a, b)=>{if(a.eval === "pending") return -1; if(a.eval === "false") return 1; if(a.eval === "true" && b.eval === "pending") return 1;})
+        this.props.predictions.sort((a, b)=>{
+            if(a.eval === "pending") return -1; 
+            if(a.eval === "false") return 1; 
+            if(a.eval === "true" && b.eval === "pending")
+            return 1;})
         // return (<div>{this.props.predictions}</div>)
         return(<div className="list">{
         (this.props.predictions.map((item, index)=>{return <div className="item">
             
             {/* <br/> */}
-            <input className="listBit" type="button" id={index} value="true" onClick={(e)=>{models.score(e.target.id, e.target.value)}}></input>
-            <input className="listBit" type="button" id={index} value="false" onClick={(e)=>{models.score(e.target.id, e.target.value)}}></input>
+            <input className="listBit" type="button" id={item.tag} value="true" onClick={(e)=>{models.score(e.target.id, e.target.value)}}></input>
+            <input className="listBit" type="button" id={item.tag} value="false" onClick={(e)=>{models.score(e.target.id, e.target.value)}}></input>
             <span className="listBit">{item.confidence}% </span>
             <span className="score">{item.eval === "true" ? <span className="greenish">{item.eval}</span> : 
             item.eval === "false" ? <span className="redish">{item.eval}</span> :
